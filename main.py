84cf842e9cc05007f4a028e42141bf2810780a05
Haller125 <erokkabash1@gmail.com>
2023-09-20 21:21:27 +0500

    merkle_tree

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+from Blockchain import Blockchain
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+blockchain = Blockchain()
 
+print("Genesis Block:", blockchain.chain[0])
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+blockchain.add_new_block(transactions=[{'sender': 'Alice', 'recipient': 'Bob', 'amount': 50}])
+blockchain.add_new_block(transactions=[{'sender': 'Bob', 'recipient': 'Charlie', 'amount': 30}])
 
+blockchain.mine_block()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for block in blockchain.chain:
+    print("Block:", block)
