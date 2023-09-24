8b2ad679e2c530f350428ee7dfd6f92ea04f7001
Bektas Turzhanov <bektasturzanov@gmail.com>
2023-09-24 12:19:01 +0600

    error handling and better transaction handling

--- Blockchain.py
+++ Blockchain.py
@@ -4,6 +4,7 @@ class Blockchain:
     def __init__(self):
         self.chain = []
         self.mempool = []
+        self.balances = {}
         self.initialize_genesis_block()
 
     def initialize_genesis_block(self):
@@ -44,6 +45,23 @@ class Blockchain:
     def add_transaction_to_mempool(self, transaction):
         # In real-world, you'll verify the transaction first.
         # For example, check if the sender has enough balance.
+        sender = transaction['sender']
+        recipient = transaction['recipient']
+        amount = transaction['amount']
+    
+        # Check if the sender has enough balance
+        if sender not in self.balances:
+            self.balances[sender] = 0
+        if self.balances[sender] < amount:
+            print(f"Transaction failed! {sender} has insufficient funds.")
+            return
+    
+        # Check for negative transactions
+        if amount <= 0:
+            print("Transaction failed! Amount should be positive.")
+            return
+
+        # If all checks pass, add transaction to mempool
         self.mempool.append(transaction)
 
     def mine_block(self, difficulty=4):
@@ -66,5 +84,23 @@ class Blockchain:
 
         self.add_block(new_block)
 
-        # Clear the mempool
+        # Update balances based on the transactions in the mined block
+        for tx in new_block.transactions:
+            sender = tx['sender']
+            recipient = tx['recipient']
+            amount = tx['amount']
+            
+            # Deduct from sender's balance
+            if sender in self.balances:
+                self.balances[sender] -= amount
+            else:
+                self.balances[sender] = -amount
+            
+            # Add to recipient's balance
+            if recipient in self.balances:
+                self.balances[recipient] += amount
+            else:
+                self.balances[recipient] = amount
+
+        # Clear the mempool after mining
         self.mempool = []
